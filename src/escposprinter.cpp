2f41c4db895a0882672e6918af29627fbd14d165
MORIA-BASE\Den <dendudko@gmail.com>
2023-03-31 13:43:35 +0300

    Tested on Custom VKP80 II added methods for printing images, BarCodes PDF414, setting printing margins, font size, getting printer status i'm not sure that it's worked on other models esc/pos printers. It should be tested

--- src/escposprinter.cpp
+++ src/escposprinter.cpp
@@ -2,13 +2,14 @@
 
 #include <QIODevice>
 #include <QDataStream>
+#include <QImage>
 
 #include <QLoggingCategory>
 
-#include <QTextCodec>
-
 Q_LOGGING_CATEGORY(EPP, "esc_pos")
-
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+#include <QTextCodec>
+#endif
 static const char ESC = 0x1B;
 static const char GS = 0x1D;
 
@@ -17,21 +18,102 @@ using namespace EscPosQt;
 EscPosPrinter::EscPosPrinter(QIODevice *device, QObject *parent) : QObject(parent)
   , m_device(device)
 {
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     m_codec = QTextCodec::codecForLocale();
-    connect(m_device, &QIODevice::readyRead, this, [=] {
+#else
+    m_codec = QStringEncoder();
+#endif
+    /*connect(m_device, &QIODevice::readyRead, this, [=] {
         const QByteArray data = m_device->readAll();
         qCDebug(EPP) << "GOT" << data << data.toHex();
-    });
+    });*/
 }
 
 EscPosPrinter::EscPosPrinter(QIODevice *device, const QByteArray &codecName, QObject *parent) : QObject(parent)
   , m_device(device)
 {
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     m_codec = QTextCodec::codecForName(codecName);
-    connect(m_device, &QIODevice::readyRead, this, [=] {
+#else
+    m_codec = QStringEncoder(codecName);
+#endif
+    /*connect(m_device, &QIODevice::readyRead, this, [=] {
         const QByteArray data = m_device->readAll();
         qCDebug(EPP) << "GOT" << data << data.toHex();
-    });
+    });*/
+}
+
+EscPosPrinter &EscPosPrinter::setFontSize(int x, int y)
+{
+  char str[] = { GS, '!',char( (((x<=0)?0:(x>=8)?7:(x-1))<<4) | ((y<=0)?0:(y>=8)?7:(y-1)) ) };
+  qCDebug(EPP) << "setFontSize " << QByteArray(str, sizeof(str)) << QByteArray(str, sizeof(str)).toHex();
+  write(str, sizeof(str));
+  return *this;
+}
+
+EscPosPrinter &EscPosPrinter::operator<<(PrintMode i)
+{
+  char str[] = { ESC, 0, '\x01' };
+  printModes.setFlag(i);
+  switch(i)
+  {
+  case PrintModeNone:
+    return *this;
+  case PrintModeDoubleHeight:
+  case PrintModeDoubleWidth:
+    str[0] = GS;
+    str[1] = '!';
+    str[2] = (printModes.testFlag(PrintModeDoubleWidth)?0x00:0x10)|(printModes.testFlag(PrintModeDoubleHeight)?0x00:0x01);
+    break;
+  case PrintModeFont2:
+    str[1] = '\x4D';
+    str[2] = 49;
+    break;
+  case PrintModeEmphasized:
+    str[1] = '\x45';
+    break;
+  case PrintModeItalic:
+    str[1] = '4';
+    break;
+  case PrintModeUnderline:
+    str[1] = '-';
+    break;
+  }
+  qCDebug(EPP) << "+printMode " << QByteArray(str, sizeof(str)) << QByteArray(str, sizeof(str)).toHex();
+  write(str, sizeof(str) );
+  return *this;
+}
+EscPosPrinter &EscPosPrinter::operator>>(PrintMode i)
+{
+  char str[] = { ESC, 0, '\x00' };
+  printModes.setFlag(i, false);
+  switch(i)
+  {
+  case PrintModeNone:
+    return *this;
+  case PrintModeDoubleHeight:
+  case PrintModeDoubleWidth:
+    str[0] = GS;
+    str[1] = '!';
+    str[2] = (printModes.testFlag(PrintModeDoubleWidth)?0x00:0x10)|(printModes.testFlag(PrintModeDoubleHeight)?0x00:0x01);
+    break;
+  case PrintModeFont2:
+    str[1] = '\x4D';
+    str[2] = 48;
+    break;
+  case PrintModeEmphasized:
+    str[1] = '\x45';
+    break;
+  case PrintModeItalic:
+    str[1] = '4';
+    break;
+  case PrintModeUnderline:
+    str[1] = '-';
+    break;
+  }
+  qCDebug(EPP) << "-printMode " << QByteArray(str, sizeof(str)) << QByteArray(str, sizeof(str)).toHex();
+  write(str, sizeof(str) );
+  return *this;
 }
 
 EscPosPrinter &EscPosPrinter::operator<<(PrintModes i)
@@ -73,25 +155,63 @@ EscPosPrinter &EscPosPrinter::operator<<(const EscPosPrinter::QRCode &qr)
     return *this;
 }
 
+EscPosPrinter &EscPosPrinter::operator<<(const BarCode &bc)
+{
+  const char str[] = { GS, 'w', char(bc.width>255?255:bc.width<1?1:bc.width)
+                       , GS, 'h', char(bc.height>255?255:bc.height<12?12:bc.height)
+                       , GS, '\x6b', char(bc.model) };
+  const char strEnd[] = { 0 };
+  write(str, sizeof(str));
+  write(bc.data);
+  write(strEnd, sizeof(strEnd));
+  return *this;
+};
+
+EscPosPrinter &EscPosPrinter::operator<<(const PDF417BarCode &bc)
+{
+  const char str[] = { GS, '(', 'k', 0x03, 0x00, 0x30, 0x41, char(bc.colCount>255?255:bc.colCount<1?1:bc.colCount)
+                     , GS, '(', 'k', 0x03, 0x00, 0x30, 0x42, char(bc.rowCount>255?255:bc.rowCount<1?1:bc.rowCount)
+                     , GS, '(', 'k', 0x03, 0x00, 0x30, 0x43, char(bc.width>255?255:bc.width<1?1:bc.width)
+                     , GS, '(', 'k', 0x03, 0x00, 0x30, 0x44, char(bc.height>255?255:bc.height<12?12:bc.height)
+                     , GS, '(', 'k', 0x03, 0x00, 0x30, 0x45, 0x30, 0x02
+                     , GS, '(', 'k', char((bc.data.length()+3)&0xff), char(((bc.data.length()+3)>>8)&0xff), 0x30, 0x50, 0x30 };
+  const char strEnd[] = { GS, '(', 'k', 0x03, 0x00, 0x30, 0x51, 0x30 };
+  write(str, sizeof(str));
+  write(bc.data);
+  write(strEnd, sizeof(strEnd));
+  return *this;
+};
+
 EscPosPrinter &EscPosPrinter::operator<<(const QString &text)
 {
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     qCDebug(EPP) << "string" << text << text.toLatin1() << m_codec->fromUnicode(text);
     if (m_codec) {
         write(m_codec->fromUnicode(text));
     } else {
         write(text.toLatin1());
     }
+#else
+    qCDebug(EPP) << "string" << text << text.toLatin1() << m_codec.encode(text);
+    QByteArray barr = m_codec.encode(text);
+    write( barr );
+#endif
     return *this;
 }
 
 EscPosPrinter &EscPosPrinter::operator<<(QStringView text)
 {
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     qCDebug(EPP) << "string" << text << text.toLatin1() << m_codec->fromUnicode(text);
     if (m_codec) {
         write(m_codec->fromUnicode(text));
     } else {
         write(text.toLatin1());
     }
+#else
+    qCDebug(EPP) << "string" << text << text.toLatin1() << m_codec.encode(text);
+    write(m_codec.encode(text));
+#endif
     return *this;
 }
 
@@ -109,6 +229,19 @@ EscPosPrinter &EscPosPrinter::operator<<(void (*pf)())
     return *this;
 }
 
+EscPosPrinter &EscPosPrinter::operator<<(const QImage &img)
+{
+  if(img.colorCount()==2)
+  {
+    const char str[] = { GS, '\x76', '\x30', '\x00', char(img.bytesPerLine()&0xff), char((img.bytesPerLine()>>8)&0xff), char(img.height()&0xff), char((img.height()>>8)&0xff)};
+    QByteArray imgData = QByteArray(reinterpret_cast<const char*>(img.bits()),img.sizeInBytes());
+    write(str, sizeof(str));
+    write( imgData );
+    //write("\n");
+  }
+  return *this;
+}
+
 void EscPosPrinter::write(const QByteArray &data)
 {
     m_device->write(data);
@@ -122,44 +255,50 @@ void EscPosPrinter::write(const char *data, int size)
 EscPosPrinter &EscPosPrinter::initialize()
 {
     const char str[] = { ESC, '@'};
+    qCDebug(EPP) << "init " << QByteArray(str, sizeof(str)) << QByteArray(str, sizeof(str)).toHex();
     write(str, sizeof(str));
     return *this;
 }
-
+const char* encodingNames[] = {"" , "?", "IBM 850", "?", "-", "-", "-", "-"
+                             , "-", "-", "-", "-", "-", "-", "-", "-"
+                             , "-", "IBM 866", "?", "-", "-", "-", "-", "-"
+                             , "-", "-", "-", "-", "-", "-", "-", "-"
+                             , "-", "-", "-", "-", "-", "-", "-", "ISO8859-2"
+                             , "ISO8859-15", "-", "-", "-", "-", "Windows-1250", "Windows-1251", "Windows-1253"
+                             , "Windows-1254", "Windows-1255", "Windows-1256", "Windows-1257", "Windows-1258"
+                            };
 EscPosPrinter &EscPosPrinter::encode(EscPosPrinter::Encoding codec)
 {
-    switch (codec) {
-    case EncodingPC850:
-        m_codec = QTextCodec::codecForName("IBM 850");
-        break;
-    case EncodingPC866:
-        m_codec = QTextCodec::codecForName("IBM 866");
-        break;
-    case EncodingISO8859_2:
-        m_codec = QTextCodec::codecForName("ISO8859-2");
-        break;
-    case EncodingISO8859_15:
-        m_codec = QTextCodec::codecForName("ISO8859-15");
-        break;
-    default:
-        m_codec = nullptr;
-    }
+    const char str[] = { ESC, 't', char(codec)};
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    if( size_t(codec)<sizeof(encodingNames)/sizeof(encodingNames[0]) )
+      m_codec = QTextCodec::codecForName(encodingNames[codec]);
+    else
+      m_codec = nullptr;
 
     if (!m_codec) {
         qCWarning(EPP) << "Could not find a Qt Codec for" << codec;
     }
-
-    const char str[] = { ESC, 't', char(codec)};
     qCDebug(EPP) << "encoding" << codec << QByteArray(str, sizeof(str)).toHex() << m_codec;
+#else
+    if( size_t(codec)<sizeof(encodingNames)/sizeof(encodingNames[0]) )
+        m_codec = QStringEncoder(encodingNames[codec]);
+    else
+        m_codec = QStringEncoder();
+    qCDebug(EPP) << "encoding" << codec << QByteArray(str, sizeof(str)) << QByteArray(str, sizeof(str)).toHex() << m_codec.name();
+#endif
     write(str, sizeof(str));
     return *this;
 }
 
 EscPosPrinter &EscPosPrinter::mode(EscPosPrinter::PrintModes pm)
 {
-    qCDebug(EPP) << "print modes" << pm;
-    const char str[] = { ESC, '!', char(pm)};
+    printModes = pm;
+    qCDebug(EPP) << "print modes" << printModes;
+    const char str[] = { ESC, '!', char(printModes)};
     write(str, sizeof(str));
+    transmitLeftMargin();
+    transmitPrintingWidth();
     return *this;
 }
 
@@ -180,6 +319,15 @@ EscPosPrinter &EscPosPrinter::modePage()
 EscPosPrinter &EscPosPrinter::partialCut()
 {
     const char str[] = { ESC, 'm'};
+    qCDebug(EPP) << "partialCut " << QByteArray(str, sizeof(str)) << QByteArray(str, sizeof(str)).toHex();
+    write(str, sizeof(str));
+    return *this;
+}
+
+EscPosPrinter &EscPosPrinter::totalCut()
+{
+    const char str[] = { ESC, 'i'};
+    qCDebug(EPP) << "totalCut " << QByteArray(str, sizeof(str)) << QByteArray(str, sizeof(str)).toHex();
     write(str, sizeof(str));
     return *this;
 }
@@ -187,6 +335,15 @@ EscPosPrinter &EscPosPrinter::partialCut()
 EscPosPrinter &EscPosPrinter::printAndFeedPaper(quint8 n)
 {
     const char str[] = { ESC, 'J', char(n)};
+    qCDebug(EPP) << "printAndFeedPaper " << QByteArray(str, sizeof(str)) << QByteArray(str, sizeof(str)).toHex();
+    write(str, sizeof(str));
+    return *this;
+}
+
+EscPosPrinter &EscPosPrinter::presentTicket(quint8 stepCount )//1step == 7.3 mm
+{
+    const char str[] = { GS, '\x65', '\x03', char(stepCount)};
+    qCDebug(EPP) << "presentTicket " << QByteArray(str, sizeof(str)) << QByteArray(str, sizeof(str)).toHex();
     write(str, sizeof(str));
     return *this;
 }
@@ -194,22 +351,71 @@ EscPosPrinter &EscPosPrinter::printAndFeedPaper(quint8 n)
 EscPosPrinter &EscPosPrinter::align(EscPosPrinter::Justification i)
 {
     const char str[] = { ESC, 'a', char(i)};
-    qCDebug(EPP) << "justification" << i << QByteArray(str, 3).toHex();
-    write(str, 3);// TODO doesn't work on DR700
+    qCDebug(EPP) << "justification" << i << QByteArray(str, sizeof(str)) << QByteArray(str, sizeof(str)).toHex();
+    write(str, sizeof(str));// TODO doesn't work on DR700
     return *this;
 }
 
 EscPosPrinter &EscPosPrinter::paperFeed(int lines)
 {
     const char str[] = { ESC, 'd', char(lines)};
-    qCDebug(EPP) << "line feeds" << lines << QByteArray(str, 3).toHex();
-    write(str, 3);
+    qCDebug(EPP) << "line feeds" << lines << QByteArray(str, sizeof(str)).toHex();
+    write(str, sizeof(str));
     return *this;
 }
 
-void EscPosPrinter::getStatus()
+void EscPosPrinter::transmitLeftMargin()
 {
+  const char str[] = { GS, 'L', char(leftMargin&0xff), char((leftMargin>>8)&0xff)};
+  qCDebug(EPP) << "left margin" << leftMargin << QByteArray(str, sizeof(str)).toHex();
+  write(str, sizeof(str));
+}
+void EscPosPrinter::transmitPrintingWidth()
+{
+  const char str[] = { GS, 'W', char(printingWidth&0xff), char((printingWidth>>8)&0xff)};
+  qCDebug(EPP) << "printing width" << printingWidth << QByteArray(str, sizeof(str)).toHex();
+  write(str, sizeof(str));
+}
 
+EscPosPrinter &EscPosPrinter::setLeftMargin(int value)
+{
+  leftMargin = value;
+  transmitLeftMargin();
+  return *this;
+}
+EscPosPrinter &EscPosPrinter::setPrintingWidth(int value)
+{
+  printingWidth = value;
+  transmitPrintingWidth();
+  return *this;
+}
+
+EscPosPrinter::PrinterStatuses EscPosPrinter::getStatus()
+{
+  const char str[] = { '\x10', '\x04', '\x14'};
+  write(str, sizeof(str));
+  EscPosPrinter::PrinterStatuses result;
+  //if(m_device->bytesAvailable()>=6)
+  {
+    QByteArray data = m_device->read(6);
+    if(data.length()>=6)
+    {
+      result.setFlag( PrinterStatusOnline );
+      qDebug() << int(data[0]) << int(data[1]);
+      if(data[2]&0x01)
+        result.setFlag( PrinterStatusPaperEnd );
+      if(data[2]&0x04)
+        result.setFlag( PrinterStatusPaperNearEnd );
+      if(data[3]&0x03)
+        result.setFlag( PrinterStatusCoverOpened );
+      if(data[4]&0x0b || data[5]&0x4d )
+        result.setFlag( PrinterStatusOtherDeviceError );
+      if(data[4]&0x40)
+        result.setFlag( PrinterStatusPaperJam );
+    }
+  }
+  write(str, sizeof(str)-1 );
+  return result;
 }
 
 EscPosPrinter::QRCode::QRCode(EscPosPrinter::QRCode::Model model, int moduleSize, EscPosPrinter::QRCode::ErrorCorrection erroCorrection, const QByteArray &_data)
@@ -223,7 +429,6 @@ EscPosPrinter::QRCode::QRCode(EscPosPrinter::QRCode::Model model, int moduleSize
     const char mT[] = {
         GS, '(', 'k', 0x04, 0x00, 0x31, 0x41, char(model), 0x00};
     data.append(mT, sizeof(mT));
-
 
     // Module Size f167
     const char mS[] = {
@@ -254,3 +459,4 @@ EscPosPrinter::QRCode::QRCode(EscPosPrinter::QRCode::Model model, int moduleSize
         GS, '(', 'k', 0x03, 0x00, 0x31, 0x51, 0x30};
     data.append(pT, sizeof(pT));
 }
+
--- src/escposprinter.h
+++ src/escposprinter.h
@@ -2,10 +2,15 @@
 #define ESCPOSPRINTER_H
 
 #include <QObject>
+#define EscPosQt6_EXPORTS 1
 #include <escposexports.h>
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+class QTextCodec;
+#else
+#include <QStringConverter>
+#endif
 
 class QIODevice;
-class QTextCodec;
 
 namespace EscPosQt {
 
@@ -29,7 +34,29 @@ public:
             H, // 30%
         };
         QRCode(EscPosPrinter::QRCode::Model model, int moduleSize, EscPosPrinter::QRCode::ErrorCorrection erroCorrection, const QByteArray &_data);
-
+        QByteArray data;
+    };
+    struct BarCode {
+        enum Type {
+          UPCA = 1, //only digits, len in (11,12)
+          UPC_E, //only digits, len in (11,12)
+          EAN13, //only digits, len in (11-13)
+          EAN8, //only digits, len in (1-7)
+          CODE39, //alpanumeric(latin) + (\x32,\x36,\x37,\x43,\x45,\x46,\x47), len in (1-12)
+          ITF25, //only digits, len in (1-12)
+          CODABAR, //alpanumeric(latin) + (\x36,\x43,\x45,\x46,\x47), len in (1-12)
+          CODE93, //alpanumeric(latin) + (\x32,\x36,\x37,\x43,\x45,\x46,\x47), len in (1-12)
+          CODE128A, //char in (0-127), len in (1-12)
+        };
+        BarCode(EscPosPrinter::BarCode::Type model, int width, int height, const QByteArray &_data):model(model),width(width),height(height),data(_data){};
+        EscPosPrinter::BarCode::Type model;
+        int width, height;
+        QByteArray data;
+    };
+    struct PDF417BarCode {
+        PDF417BarCode(int colCount, int rowCount, int width, int height, const QByteArray &_data)
+          :colCount(colCount), rowCount(rowCount), width(width), height(height), data(_data){};
+        int colCount, rowCount, width, height;
         QByteArray data;
     };
 
@@ -39,6 +66,7 @@ public:
         PrintModeEmphasized = 0x08,
         PrintModeDoubleHeight = 0x10,
         PrintModeDoubleWidth = 0x20, // 16char on mini, 24 on 80mm
+        PrintModeItalic = 0x40,
         PrintModeUnderline = 0x80,
     };
     Q_ENUM(PrintMode)
@@ -68,19 +96,39 @@ public:
         EncodingPC852 = 18,
         EncodingISO8859_2 = 39,// Qt supported
         EncodingISO8859_15 = 40,// Qt supported
+        EncodingWPC1250 = 45,// Qt supported
+        EncodingWPC1251 = 46,// Qt supported
+        EncodingWPC1253 = 47,// Qt supported
+        EncodingWPC1254 = 48,// Qt supported
+        EncodingWPC1255 = 49,// Qt supported
+        EncodingWPC1256 = 50,// Qt supported
+        EncodingWPC1257 = 51,// Qt supported
+        EncodingWPC1258 = 52,// Qt supported
     };
     Q_ENUM(Encoding)
 
     struct _feed { int _lines; };
     inline static _feed feed(int __lines) { return { __lines }; }
+private:
+    int leftMargin = 0;
+    int printingWidth = 0;
+    PrintModes printModes;
+    void transmitLeftMargin();
+    void transmitPrintingWidth();
+public:
 
     EscPosPrinter &operator<<(PrintModes i);
+    EscPosPrinter &operator<<(PrintMode i);
+    EscPosPrinter &operator>>(PrintMode i);
     EscPosPrinter &operator<<(Justification i);
     EscPosPrinter &operator<<(Encoding i);
     EscPosPrinter &operator<<(_feed lines);
     EscPosPrinter &operator<<(const char *s);
     EscPosPrinter &operator<<(const QByteArray &s);
     EscPosPrinter &operator<<(const QRCode &qr);
+    EscPosPrinter &operator<<(const BarCode &bc);
+    EscPosPrinter &operator<<(const PDF417BarCode &bc);
+    EscPosPrinter &operator<<(const QImage &img); // img.width % 32 == 0
     /*!
      * The UTF-8 string will be encoded with QTextCodec
      * if one of the Qt supported encodings is selected.
@@ -103,15 +151,33 @@ public:
     EscPosPrinter &modeStandard();
     EscPosPrinter &modePage();
     EscPosPrinter &partialCut();
+    EscPosPrinter &totalCut();
     EscPosPrinter &printAndFeedPaper(quint8 n = 1);
+    EscPosPrinter &presentTicket(quint8 stepCount);
     EscPosPrinter &align(Justification i);
     EscPosPrinter &paperFeed(int lines = 1);
+    EscPosPrinter &setLeftMargin(int value);
+    EscPosPrinter &setPrintingWidth(int value);
+    EscPosPrinter &setFontSize(int x, int y);
 
-public Q_SLOTS:
-    void getStatus();
+    enum PrinterStatus {PrinterStatusOnline = 0x01,
+                       PrinterStatusCoverOpened = 0x02,
+                       PrinterStatusPaperEnd  = 0x04,
+                       PrinterStatusPaperNearEnd  = 0x08,
+                       PrinterStatusPaperJam  = 0x10,
+                       PrinterStatusOtherDeviceError = 0x20};
+    Q_DECLARE_FLAGS(PrinterStatuses, PrinterStatus);
+    PrinterStatuses getStatus();
+
+//public Q_SLOTS:
+    //void getStatus();
 
 private:
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QTextCodec *m_codec = nullptr;
+#else
+    QStringEncoder m_codec;
+#endif
     QIODevice *m_device;
 };
 
